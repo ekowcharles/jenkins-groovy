1c0f8307d21c5a1cf07e545242d0c14ad9fec37e
Charles Boadu <ekowcharles@gmail.com>
2020-01-01 01:41:23 -0600

    Fix typo in repo name

--- job.groovy
+++ job.groovy
@@ -1,4 +1,4 @@
-def project = 'ekowcharles/jenkins-job-repo'
+def project = 'ekowcharles/jenkins-repo'
 def branchApi = new URL("https://api.github.com/repos/${project}/branches")
 def branches = new groovy.json.JsonSlurper().parse(branchApi.newReader())
 branches.each {
